3908471f3a027e69eafb4b549a402220ac8da330
MacBook Air <macbookair@MacBooks-MacBook-Air.local>
2024-07-12 13:48:17 +0700

    Slicing UI to Nuxt 3

---
+++ app.vue
@@ -0,0 +1,19 @@
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  head: {
+    title: 'Positivus',
+    meta: [
+      { charset: 'UTF-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1.0' }
+    ]
+  }
+}
+</script>
